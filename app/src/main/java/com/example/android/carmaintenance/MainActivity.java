db45fc95956ce79215430678915d49e7f7b4638a
sshmm <s.shawky92@gmail.com>
2019-03-05 02:46:10 +0200

    add services fragment

--- app/src/main/java/com/example/android/carmaintenance/MainActivity.java
+++ app/src/main/java/com/example/android/carmaintenance/MainActivity.java
@@ -203,7 +203,13 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            if (position != 0){
+                return ServiceFragment.newInstance(position + 1);
+
+            }else {
+                return PlaceholderFragment.newInstance(position + 1);
+
+            }
         }
 
         @Override
